1e2d53d9671944ba8cc4d5116dbc1075af9b0fb9
TypicalDefender <sdalabehra@rippling.com>
2025-01-19 14:34:58 +0530

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2563eb',
+        'secondary': '#475569'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            }
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
